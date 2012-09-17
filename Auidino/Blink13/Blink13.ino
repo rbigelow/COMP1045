418c96cf8bb35cab03c57258ea76d14ce6d97646
R B <Ross.Bigelow@georgiancollege.ca>
2012-09-17 16:20:54 -0400

    Minor revisions
    Updated .gitignore, and edited comments in Blink13.

--- Auidino/Blink13/Blink13.ino
+++ Auidino/Blink13/Blink13.ino
@@ -1,12 +1,10 @@
 /*
   Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
+  Turns on an LED on for one second, then off for one second, repeatedly. 
   This example code is in the public domain.
  */
  
 // Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
 int led = 13;
 
 // the setup routine runs once when you press reset:
